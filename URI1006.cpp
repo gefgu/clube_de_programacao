535066301ecbd40b93da5e92806327698daeebdd
Gustavo Santos <gefgu@hotmail.com>
2023-07-12 15:27:08 -0300

    feat: add URI1006

---
+++ URI1006.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+
+  float a, b, c;
+
+  scanf("%f %f %f", &a, &b, &c);
+
+  printf("MEDIA = %.1f\n", (a * 2 + b * 3 + c * 5) / (2 + 3 + 5));
+
+  return 0;
+}
